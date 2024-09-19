aa726f5448f30fa12b38d6e4830315ad845cf5d1
rhatos <rhatking@proton.me>
2024-09-19 23:05:05 +0200

    THE FIX

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 import pymongo
 
 # Database
-db_client = pymongo.MongoClient('0.0.0.0', 27017, username="capstone2024", password="changeme123")
+db_client = pymongo.MongoClient('bpe-mongodb', 27017, username="capstone2024", password="changeme123")
 database = db_client.db
